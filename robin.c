a71df9afbaedd6824611a1c04ca468f6a2788f73
AymanUzayr <68193240+AymanUzayr@users.noreply.github.com>
2024-07-18 03:52:01 +0530

    Update robin.c

--- robin.c
+++ robin.c
@@ -1,74 +1,60 @@
 #include <stdio.h>
 
-struct process {
-    int burst;  // Burst time (execution time) of the process
-    int wait;   // Waiting time of the process
-    int comp;   // Time the process has been completed (used for tracking progress)
-    int f;      // Flag to check if the process is finished
-} p[20] = {0, 0};  // Initialize the array of process structures with zero values
-
 int main() {
-    int n, i, j, totalwait = 0, totalturn = 0, quantum, flag = 1, time = 0;
+    int n, bt[10], wt[10], tat[10], rem_bt[10], pno[10];
+    int i, j, quantum, t = 0, count = 0;
+    float avg_wt = 0, avg_tat = 0;
 
-    // Read number of processes
-    printf("\nEnter The No Of Process :");
+    printf("\nEnter the number of processes: ");
     scanf("%d", &n);
 
-    // Read the time quantum
-    printf("\nEnter The Quantum time (in ms) :");
+    printf("\nEnter the burst time of each process:\n");
+    for (i = 0; i < n; i++) {
+        printf("P%d: ", i);
+        scanf("%d", &bt[i]);
+        rem_bt[i] = bt[i]; // Copy burst times to remaining burst times
+        pno[i] = i; // Assigning process numbers
+    }
+
+    printf("\nEnter the time quantum: ");
     scanf("%d", &quantum);
 
-    // Read burst times for each process
-    for (i = 0; i < n; i++) {
-        printf("Enter The Burst Time (in ms) For Process #%2d :", i + 1);
-        scanf("%d", &p[i].burst);
-        p[i].f = 1;  // Initialize flag to 1 (indicating the process is not finished)
-    }
-
-    // Print header for order of execution
-    printf("\nOrder Of Execution \n");
-    printf("\nProcess Starting Ending Remaining");
-    printf("\n\t\tTime \tTime \t Time");
-
-    // Round Robin scheduling
-    while (flag == 1) {
-        flag = 0;
+    while (1) {
+        count = 0;
         for (i = 0; i < n; i++) {
-            if (p[i].f == 1) {  // If process is not finished
-                flag = 1;
-                j = quantum;
-
-                if ((p[i].burst - p[i].comp) > quantum) {
-                    p[i].comp += quantum;
+            if (rem_bt[i] > 0) {
+                count++;
+                if (rem_bt[i] > quantum) {
+                    t += quantum;
+                    rem_bt[i] -= quantum;
                 } else {
-                    p[i].wait = time - p[i].comp;
-                    j = p[i].burst - p[i].comp;
-                    p[i].comp = p[i].burst;
-                    p[i].f = 0;
+                    t += rem_bt[i];
+                    wt[i] = t - bt[i];
+                    tat[i] = t;
+                    rem_bt[i] = 0;
                 }
-
-                // Print the details of the current execution
-                printf("\nprocess # %-3d %-10d %-10d %-10d", i + 1, time, time + j, p[i].burst - p[i].comp);
-                time += j;
             }
         }
+        if (count == 0)
+            break;
     }
 
-    // Print header for process details
-    printf("\n\n------------------");
-    printf("\nProcess \t Waiting Time TurnAround Time ");
-
-    // Calculate total waiting time and turnaround time
+    // Calculate average waiting time and average turnaround time
     for (i = 0; i < n; i++) {
-        printf("\nProcess # %-12d%-15d%-15d", i + 1, p[i].wait, p[i].wait + p[i].burst);
-        totalwait += p[i].wait;
-        totalturn += p[i].wait + p[i].burst;
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+    avg_wt /= n;
+    avg_tat /= n;
+
+    // Print the results
+    printf("\nProcess No\tBurst Time\tWait Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++) {
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", pno[i], bt[i], wt[i], tat[i]);
     }
 
-    // Print average waiting time and turnaround time
-    printf("\n\nAverage\n------------------ ");
-    printf("\nWaiting Time: %fms", totalwait / (float)n);
-    printf("\nTurnAround Time : %fms\n\n", totalturn / (float)n);
+    printf("\nAverage Waiting Time = %.2f\n", avg_wt);
+    printf("Average Turnaround Time = %.2f\n", avg_tat);
 
     return 0;
 }
