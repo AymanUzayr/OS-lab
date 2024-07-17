c21c190f06aba7dc833e23a07e94c34723844e9e
AymanUzayr <68193240+AymanUzayr@users.noreply.github.com>
2024-07-18 02:38:10 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int i=0,pno[10],bt[10],n,wt[10],temp=0,j,tt[10];
-    float sum=0,at=0;
+    int i=0,pno[10],bt[10],n,wt[10],temp=0,j,tat[10];
+    float sum=0,ttat=0;
 
     printf("\n Enter the no of process ");
     scanf("%d",&n);
@@ -47,14 +47,14 @@ int main()
     printf("\n process no \t burst time\t waiting time \t turn around time\n");
     for(i=0;i<n;i++)
     {
-        tt[i]=bt[i]+wt[i]; // Turnaround time = Burst time + Waiting time
-        at+=tt[i]; // Sum of turnaround times
-        printf("\n p%d\t\t%d\t\t%d\t\t%d",pno[i],bt[i],wt[i],tt[i]);
+        tat[i]=bt[i]+wt[i]; // Turnaround time = Burst time + Waiting time
+        ttat+=tat[i]; // Sum of turnaround times
+        printf("\n p%d\t\t%d\t\t%d\t\t%d",pno[i],bt[i],wt[i],tat[i]);
     }
 
     // Printing average waiting time and average turnaround time
     printf("\n\n\t Average waiting time: %f", sum/n);
-    printf("\n\t Average turn around time: %f\n", at/n);
+    printf("\n\t Average turn around time: %f\n", ttat/n);
 
     return 0;
 }
